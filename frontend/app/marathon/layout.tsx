18d32a9896bcfd60116938652a72119edbdff8fe
reinholdkamilo <reinhold.kamilo@gmail.com>
2026-07-15 23:24:01 +1000

    style: load marathon v2 presentation

--- frontend/app/marathon/layout.tsx
+++ frontend/app/marathon/layout.tsx
@@ -1,5 +1,6 @@
 import type { ReactNode } from "react";
 import "./marathon.css";
+import "./marathon-v2.css";
 
 export default function MarathonLayout({ children }: { children: ReactNode }) {
   return children;
